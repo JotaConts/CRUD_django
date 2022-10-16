8381076233aac4d31ecb2737fc7bf3cee28373f0
JotaConts <jotape.desenfocado@gmail.com>
2022-10-16 16:10:24 -0300

    Conectando con la base de datos y ajustes finales

--- CRUD_django/libreria/urls.py
+++ CRUD_django/libreria/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('nosotros/', views.nosotros, name='nosotros'),
     path('libros/', views.libros, name='libros'),
-    path('crear/', views.crear, name='crear'),
-    path('editar/', views.editar, name='editar'),
-    path('eliminar/<int:id>', views.eliminar, name='eliminar')
+    path('libros/crear/', views.crear, name='crear'),
+    path('libros/editar/', views.editar, name='editar'),
+    path('libros/editar/<int:id>', views.editar, name='editar' ),
+    path('eliminar/<int:id>', views.eliminar, name='eliminar'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # este último comando es para que se muestren las imágenes por pantalla
